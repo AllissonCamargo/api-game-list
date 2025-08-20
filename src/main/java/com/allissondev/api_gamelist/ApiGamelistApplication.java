ba09522070d9f532a481909d16a0c8f240999cce
Allisson Camargo <allisson.camargopr@gmail.comgit config --global user.email allisson.camargopr@gmail.comgit config --global user.email allisson.camargopr@gmail.com>
2025-08-20 16:14:11 -0300

    create properties
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/allissondev/api_gamelist/ApiGamelistApplication.java
@@ -0,0 +1,13 @@
+package com.allissondev.api_gamelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGamelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGamelistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allissondev/api_gamelist/ApiGamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allissondev.api_gamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
