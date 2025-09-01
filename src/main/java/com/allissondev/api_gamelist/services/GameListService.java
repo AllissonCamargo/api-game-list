383a756aa52d5d7f91e86d752031cb72e716c81e
Allisson Camargo <allisson.camargopr@gmail.comgit config --global user.email allisson.camargopr@gmail.comgit config --global user.email allisson.camargopr@gmail.com>
2025-09-01 16:02:41 -0300

    Move games on list

--- src/main/java/com/allissondev/api_gamelist/services/GameListService.java
+++ src/main/java/com/allissondev/api_gamelist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.allissondev.api_gamelist.services;
 
 import com.allissondev.api_gamelist.dto.GameListDTO;
 import com.allissondev.api_gamelist.entities.GameList;
+import com.allissondev.api_gamelist.projections.GameMinProjection;
 import com.allissondev.api_gamelist.repositories.GameListRepository;
+import com.allissondev.api_gamelist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,11 +15,28 @@ import java.util.List;
 public class GameListService {
 
     @Autowired
+    private GameRepository gameRepository;
+
+    @Autowired
     private GameListRepository gameListRepository;
 
     @Transactional
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = Math.min(sourceIndex, destinationIndex);
+        int max = Math.max(sourceIndex, destinationIndex);
+        for (int i = min; i < max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
+
     }
 }
